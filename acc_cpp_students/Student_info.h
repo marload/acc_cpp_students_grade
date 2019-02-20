69692d90721be41656f97e1ee9d865a961365554
olramde <rladhkstn8@gmail.com>
2019-02-20 15:04:04 +0900

    first commit

---
+++ acc_cpp_students/Student_info.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef GUARD_Student_info
+#define GUARD_Student_info
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct Student_info {
+	std::string name;
+	double midterm, final;
+	std::vector<double> homework;
+};
+
+bool compare(const Student_info&, const Student_info&);
+std::istream& read(std::istream&, Student_info&);
+std::istream& read_hw(std::istream&, std::vector<double>&);
+
+#endif // !GUARD_Student_info
---
+++ acc_cpp_students/median.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef GUARD_median_h
+#define GUARD_median_h
+
+#include <vector>
+
+double median(std::vector<double>);
+
+#endif // !GUARD_median_h
